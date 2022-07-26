fb3f04e2aa1a51403b22f86cdbf5384148f49949
Syed Ameenuddin <ameen.syed124@gmail.com>
2022-07-26 18:34:24 +0530

    Update userinfo.py

--- userinfo.py
+++ userinfo.py
@@ -1,35 +1,17 @@
 name = 'Syed'
 fullname = 'Syed Ameenuddin'
-phonenumber = 9360100035
+phonenumber = "xyx"
 email = 'mail2syedameenuddin@yahoo.com'
 githubacct = 'https://github.com/syedAmeenuddin?tab=repositories'
 gmail = 'https://mail.google.com/mail/u/0/#inbox'
 spotifyweb = 'https://open.spotify.com/'
 
+
+#family contacts for whatsapp message
 contact = {
-            'ammi': '+917871146253'
-            ,'mom': '+917871146253'
-            ,'mummy': '+917871146253'
-           ,'ami': '+917871146253'
-           ,'abba':'+919841362593'
-           ,'dad':'+919841362593'
-            ,'daddy':'+919841362593'
-           ,'aba':'+919841362593'
-           ,'shaju':'+918056004832'
-           ,'bhabhi':'+9190031172717'
+            'mummy': '+91xyx'
+          
+            ,'daddy':'+91xyx'
+           
            }
 
-
-
-
-
-
-
-
-
-
-# next to do
-# make jarvis analysis using face
-# auto fill userinfo using python and save the file for more use
-# ex save abba name shafi in userinfo
-# abbaname = shafi
